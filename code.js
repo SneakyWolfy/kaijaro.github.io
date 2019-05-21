81709d21f7e2465fa1ca56db8dc2c822b8a39f66
Kaijaro <50626009+Kaijaro@users.noreply.github.com>
2019-05-21 11:40:01 -0230

    test

---
+++ code.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
+
+window.onload() = function(){
+    rect(0,0,canvas.width,canvas.height, '#2a65bc');
+    rect(0,0,10,100,"#202020");
+}
