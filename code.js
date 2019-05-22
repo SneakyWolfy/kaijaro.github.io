2a8e98b9ce47d090657ca4c790119e2b82e7ffc7
Kaijaro <50626009+Kaijaro@users.noreply.github.com>
2019-05-22 13:50:27 -0230

    a

--- code.js
+++ code.js
@@ -1,7 +1,45 @@
-var canvas = document.getElementById("game");
-var ctx = canvas.getContext("2d");
+var canvas 
+var ctx 
 
-window.onload() = function(){
-    rect(0,0,canvas.width,canvas.height, '#2a65bc');
-    rect(0,0,10,100,"#202020");
+var testObj = {};
+
+window.onload = function(){
+    var canvas = document.getElementById("game");
+    var ctx = canvas.getContext("2d");
+
+    function rect(leftX,topY,width,height,drawColor) {
+        ctx.fillStyle = drawColor;
+        ctx.fillRect(leftX,topY,width,height);
+    }
+
+    function setUpTest(){
+        color="#232323"
+        rect(0,0,canvas.width,canvas.height,"#FF3333")
+        rect(0,0,40,40,color);
+    }
+
+    setUpTest()
+
+    canvas.addEventListener("mousemove",function (evt) {Getmouse(evt)})
+
+
 }
+
+function Getmouse(evt){
+    console.log(evt.clientX, evt.clientY)
+    var canvas = document.getElementById("game");
+    var ctx = canvas.getContext("2d");
+    function rect(leftX,topY,width,height,drawColor) {
+        ctx.fillStyle = drawColor;
+        ctx.fillRect(leftX,topY,width,height);
+    }
+
+    if (evt.clientX<40&&evt.clientY<40){
+        color="#ffffff";
+        rect(0,0,40,40,color);
+    } else {
+        color="#232323";
+        rect(0,0,40,40,color);
+    }
+}
+
