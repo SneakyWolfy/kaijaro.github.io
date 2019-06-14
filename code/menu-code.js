2aab52d527149758e037c26fc2f65aac8cddd5d3
Kaijaro <vulcon115@gmail.com>
2019-06-14 18:26:31 -0230

    globalized a json file

--- code/menu-code.js
+++ code/menu-code.js
@@ -7,7 +7,7 @@ var SongData;
 function loadJSON(callback) {   
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'json/song-api.json', true); 
+    xobj.open('GET', 'https://kaijaro.github.io/json/song-api.json', true); 
     xobj.onreadystatechange = function () {
     if (xobj.readyState == 4 && xobj.status == "200") {
         callback(xobj.responseText);
