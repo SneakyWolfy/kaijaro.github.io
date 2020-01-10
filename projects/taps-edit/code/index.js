1ae18e8cf1b61fd61c74a5023fc49f518d910830
Kaijaro <50626009+Kaijaro@users.noreply.github.com>
2020-01-10 12:20:05 -0330

    introducing react

--- projects/taps-edit/code/index.js
+++ projects/taps-edit/code/index.js
@@ -3,4 +3,40 @@ var $ = (id) => {
     return document.querySelector(id);
 }
 
-onload()
+//doc movment commands
+/*
+var test = new Vue({
+    el: '#testDrag',
+    methods: {
+      grab: function(event){
+        event.target.addEventListener(event.target.id+'Grab', freeze)
+      }
+    }
+  })
+
+*/
+  //doc html stuff
+
+const e = React.createElement;
+
+class testDrag extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = $('#testDrag');
+ReactDOM.render(e(testDrag), domContainer);
